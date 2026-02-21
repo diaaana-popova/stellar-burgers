e268db39de6a3bd905f6cfdff369e9c08b9e463c
Diana Popova <diaaana.popova@gmail.com>
2026-02-21 20:17:50 +0300

    Just and Cypress tests done

--- src/services/__tests__/rootReducer.test.ts
+++ src/services/__tests__/rootReducer.test.ts
@@ -4,6 +4,7 @@ import { initialState as allOrdersInitialState} from '../slices/allOrdersSlice';
 import { initialState as profileInitialState} from '../slices/profileSlice';
 import { initialState as orderInitialState} from '../slices/orderSlice';
 import { rootReducer } from '../store';
+import {expect, test} from '@jest/globals';
 
 test('инициализация root reducer', () => {
     const result = rootReducer(undefined, { type: '' });
